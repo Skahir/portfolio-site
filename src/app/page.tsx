5bc775ad720769056d127bc54f23ce21e3b931ec
Nando Reij <nando_reij@hotmail.com>
2025-08-21 23:27:45 +0200

    feat: added centering and a divider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,20 @@ export default function Home() {
   return (
     <div>
       <main>
-        <img className={styles.profile} src="https://picsum.photos/200" width="200" />
+        <div className={styles.container}>
+          <div className={styles.divider}></div>
+          <img className={styles.profile} src="https://picsum.photos/200" width="200" />
+          <div className={styles.divider}></div>
 
-        <h1>About me</h1>
-        
-        <p>
-          Hi I'm Nando. A developer who loves solving problems through clean, efficient code. Most of my work centers on building robust backends and APIs, 
-          but I'm always exploring new ways to improve performance and developer experience. What excites me most is designing solutions that 
-          make a real difference for the people who use them
-        </p>
+          <h1>About me</h1>
+          
+          <p>
+            Hi I'm Nando. A developer who loves solving problems through clean, efficient code. Most of my work centers on building robust backends and APIs, 
+            but I'm always exploring new ways to improve performance and developer experience. What excites me most is designing solutions that 
+            make a real difference for the people who use them
+          </p>
+        </div>
       </main>
-      <footer>
-
-      </footer>
     </div>
   );
 }
