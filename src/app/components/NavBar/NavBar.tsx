94a6109905cb7d8951424dd410869d25b6c7de19
Nando Reij <nando_reij@hotmail.com>
2025-08-24 22:27:54 +0200

    feat: Navbar with styles

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -4,7 +4,11 @@ import Styles from "./NavBar.module.css";
 export default function NavBar() {
     return (
         <div className={Styles.NavBar}>
-            <Link href={}
+            <Link href='#' className={Styles.Button}>Home</Link>
+            <h1 className={Styles.Divider}>/</h1>
+            <Link href='/projects' className={Styles.Button}>Projects</Link>
+            <h1 className={Styles.Divider}>/</h1>
+            <Link href='/blog' className={Styles.Button}>Blog</Link>
         </div>
     );
 }
