cc76208961013ceda4c97059ca534f302161359d
Nando Reij <nando_reij@hotmail.com>
2025-08-25 17:01:55 +0200

    fix: fixed routing to homepage

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -4,10 +4,14 @@ import Styles from "./NavBar.module.css";
 export default function NavBar() {
     return (
         <div className={Styles.NavBar}>
-            <Link href='#' className={Styles.Button}>Home</Link>
+            <Link href='/#' className={Styles.Button}>Home</Link>
+
             <h1 className={Styles.Divider}>/</h1>
+
             <Link href='/projects' className={Styles.Button}>Projects</Link>
+
             <h1 className={Styles.Divider}>/</h1>
+
             <Link href='/blog' className={Styles.Button}>Blog</Link>
         </div>
     );
