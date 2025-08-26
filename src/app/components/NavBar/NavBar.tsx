1c1181d7b7e67f26b4048b0946e795c5dcd3fd2c
Nando Reij <nando_reij@hotmail.com>
2025-08-26 23:32:32 +0200

    feat: css styling current page and :hover

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -1,18 +1,23 @@
+'use client'
+
 import Link from "next/link";
 import styles from "./NavBar.module.css";
+import { usePathname } from "next/navigation";
 
 export default function NavBar() {
+    const pathname = usePathname();
+
     return (
         <div className={styles.nav_bar}>
-            <Link href='/#' className={styles.button}>Home</Link>
+            <Link href='/#' className={`${styles.link} ${pathname === '/' ? styles.current : ''}`}>Home</Link>
 
             <h1 className={styles.divider}>/</h1>
 
-            <Link href='/projects' className={styles.button}>Projects</Link>
+            <Link href='/projects' className={`${styles.link} ${pathname === '/projects' ? styles.current : ''}`}>Projects</Link>
 
             <h1 className={styles.divider}>/</h1>
 
-            <Link href='/blog' className={styles.button}>Blog</Link>
+            <Link href='/blog' className={`${styles.link} ${pathname === '/blog' ? styles.current : ''}`}>Blog</Link>
         </div>
     );
 }
